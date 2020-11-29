d1e87117e4324fa2cddf5af6c03364ad1c38cb05
Ray Springer <ray_springer@hotmail.com>
2020-11-28 20:55:51 -0500

    Add files via upload

---
+++ hello_eye_color_2.py
@@ -0,0 +1,3 @@
+hello_dict={"first_name":"Ray", "last_name": "Springer", "eye_color": "blue"}
+print("{0} {1} has {2} eyes."
+.format(hello_dict["first_name"],hello_dict["last_name"],hello_dict["eye_color"]))
